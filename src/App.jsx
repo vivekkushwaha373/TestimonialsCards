315b7769d74114455b2f7d334586c78e3c0af267
MyGit <vivekkushwaha734@gmail.com>
2025-02-10 22:37:14 +0530

    fixed css once again

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import Testimonial from "./components/Testimonial";
 function App() {
   
   return (
-    <div className="flex flex-col w-[100vw] justify-center items-center bg-gray-200">
-      <div className="text-center h-fit">
+    <div className="flex flex-col w-[100vw] min-h-[100vh] justify-center items-center bg-gray-200">
+      <div className="text-center">
         <h1 className="text-4xl font-bold">Our Testimonials</h1> 
-        <div className="bg-violet-400 h-fit sm:w-1/5 w-4/5 mx-auto "></div>
+        <div className="bg-violet-400 sm:w-1/5 w-4/5 mx-auto "></div>
         <Testimonial reviews={reviews}></Testimonial>
       </div>
 
